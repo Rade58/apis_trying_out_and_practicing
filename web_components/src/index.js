70ec14df65ce572ad8c5b055ab78b86b86a6ca59
Rade Bajic <bajic.rade2@gmail.com>
2018-09-02 13:32:54 +0200

    pravila prilikom kreiranja custom elemenata

--- web_components/src/index.js
+++ web_components/src/index.js
@@ -6,20 +6,65 @@ CustomElementRegistry;
 //TAKVU INSTANCU ZA STRANICU, REFERENCIRA SLEDECI OBJEKAT 
 window.customElements;
 /////////////////////////////////////////////////////////////
-console.log(CustomElementRegistry);
-console.log(customElements);
-console.log(customElements instanceof CustomElementRegistry);  //------->       true
+console.log(        CustomElementRegistry                               );
+console.log(        customElements                                      );
+console.log(        customElements instanceof CustomElementRegistry     );  //------->       true
 ////////////////////////////////////////////////////////////
 
+//DA REGISTRUJEM CUSTOM ELEMENT NA WEB STRANICI, KORISTIM define METRODU KOJU IMA SVAKA 
+//CustomElementRegistry INSTANCA, STO ZNACI DA OVU METODU MOGU PRIMENITI NA customElements OBJEKTU,
+//KADA BUDE BILO POTREBNO DA DEFINISEM NOVI CUSTOM ELEMENT
+//ZA SADA CU SAMO STAMPATI POMENUTU METODU
+
+console.log(        CustomElementRegistry.prototype.define          );
+console.log(        customElements.define                           );
+
+//POMENUTOJ define METODI SE PRILIKOM POZIVANJA DODAJU SLEDECI ARGUMENTI:
+
+//      1)     DOMstring       (string imena novog custom elementa)
+//                                MORA SADRZAVATI CRTICU, ODNOSNO DASH U SVOM IMENU
+
+//      2)     KLASA (class) KOJA DEFINISE PONASANJE CUSTOM ELEMENTA
+
+//      3)     OPCIONO, KAO ARGUMENT SE MOZE DODATI I option OBJEKAT, KOJI IMA      extends    PROPERTI 
+//             POMENUTI extends PROPERTI, MORA SPECIFICIRATI BUILT-IN ELEMENT, IZ KOJEG CUSTOM ELEMENT
+//              TREBA DA NASLEDJUJE
+
+//JEDNOSTAVAN PRIMER DEFINISANJA CUSTOM ELEMENTA, KOJI BI NASLEDJIVAO OD HTMLParagraphElement
+//INSTANCE, ODNOSNO CIJI BI PROTOTIP OBJEKAT BILA HTMLParagraphElement INSTANCA, BI IZGLEDAO OVAKO
+
+customElements.define('brojac-reci', BrojacReci, {extends: 'p'});
+
+//A OVAKO BI IZGLEDALA class-A KOJA BI DEFINISALA PONASANJE, POMENUTOG CUSTOM ELEMENTA KOJI EXTENDS
+//PARAGRAF
 
 
+class BrojacReci extends HTMLParagraphElement {
+    constructor(){
+        //super SE PRVO POZIVA U KONSTRUKTORU, CIME NOVI CUSTOM ELEMENT DOBIJA SVE PROPERTIJE,
+        //KOJE IMA I JEDNA  HTMLParagraphElement INSTANCA, I PREDPOSTAVLJAM DA NA OVAKAV NACIN MOGU 
+        //REDEFINISATI NEKI PROPERTI, DA BI BIO PRILAGODJEN CUSTOM ELEMENTU
+        super();
 
-//DA POCNEM OD TOGA STO CU DEFINISATI KLASU (ES6 class SINTAKSA), KOJA CE OPISIVATI PONASANJE, NOVOG
-//CUSTOM ELEMENTA
+        //DALJE MOGU DA DEFINISEM METODE I PROPERTIJE KOJI SU KARAKTERISTICNI SAMO ZA MOJ
+        //CUSTOM ELEMENT
+    }
+}
 
-
-
-
-
-
-
+/////////////////////////////   PRAVILA PRILIKOM KREIRANJE CUSTOM ELEMNATA          ///////////////////
+//                              1)  IME CUSTOM ELEMENTA, KAO STO SAM REKAO MORA SE SASTOJATI I OD
+//                                   DASH (CRTICA) KARAKTERA; DAKLE TO MORA BITI DASH, I TO NE SME BITI
+//                                                                                 OVO (_)("DONJI" DASH)
+//                                  POMENUTO SE RADI ZBOG HTML PARSERA, KAKO BI ON 
+//                                  MOGAO RAZLIKOVATI CUSTOM OD REGULARNIH ELEMNATA
+                                    //A TAKODJE JE TIME OMOGUCEN I FORWARD COMPATIBILITY (https://en.wikipedia.org/wiki/Forward_compatibility)
+//                                  
+//                              2)  NIJE MOGUCE REGISTROVATI ISTI TAG, VISE OD JEDNOG PUTA
+//                                    AKO SE TO URADI BICE throw-OVAN DOMException
+//                                     KAD SE JDNOM BROWSER-U KAZE ZA NOVI TAG, TO JE TO, I NEMA
+//                                      POVRATKA NAZAD
+//
+//                              3)  CUSTOM ELEMENT NE SME BITI SELF-CLOSING
+//                                  DAKLE U OBZIR DOLAZE SAMO DVOSTRUKI TAGOVI
+//                                  (MORA SE SASTOJATI IZ OPENING I IZ CLOSING TAGA)
+//
