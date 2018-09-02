aa1d20fc4dcc2a72dc0ca7c5702c57d2830c4608
Rade Bajic <bajic.rade2@gmail.com>
2018-09-02 11:19:03 +0200

    customElements is instance of CustomElementRegistry

--- web_components/src/index.js
+++ web_components/src/index.js
@@ -1,4 +1,25 @@
-console.log("something");
-console.log(navigator);
+//ZA REGISTROVANJE CUSTOM EKLEMENTA, KORISTI SE KORISTI INSTANCA SLEDECE KLASE
+CustomElementRegistry;
+//KOJA PREDSTAVLJA KONTROLER CUSTOM ELEMENATAWEB DOKUMENTA
+//I TAKVA INSTANCA MI OMOGUCAVA DA REGISTRUJEM CUSTOM ELEMNT NA STRANICI
+
+//TAKVU INSTANCU ZA STRANICU, REFERENCIRA SLEDECI OBJEKAT 
+window.customElements;
+/////////////////////////////////////////////////////////////
+console.log(CustomElementRegistry);
+console.log(customElements);
+console.log(customElements instanceof CustomElementRegistry);  //------->       true
+////////////////////////////////////////////////////////////
+
+
+
+
+//DA POCNEM OD TOGA STO CU DEFINISATI KLASU (ES6 class SINTAKSA), KOJA CE OPISIVATI PONASANJE, NOVOG
+//CUSTOM ELEMENTA
+
+
+
+
+
 
 
