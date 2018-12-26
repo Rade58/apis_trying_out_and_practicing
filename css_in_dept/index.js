ae0e64ad659e4b34d3939fe3b2c8c892ecb25d77
Rade Bajic <bajic.rade2@gmail.com>
2018-12-26 15:36:15 +0100

    notes

--- css_in_dept/index.js
+++ css_in_dept/index.js
@@ -10,4 +10,10 @@ console.log('********************************************************');
 console.log(document.querySelectorAll('section[nesto^=blah]'));
 console.log(document.querySelectorAll('section[nesto$=ajovaska]'));
 
-console.log(document.querySelector('div[neki*=sand]'))
+console.log(document.querySelector('div[neki*=sand]'));
+
+console.log(document.querySelector('img[sign|=bull-cow]'))
+console.log(document.querySelector('img[sign~=bull-cow]'))
+console.log(document.querySelector('img[sign^=bull-cow]'))
+console.log(document.querySelector('img[sign$=bull-cow]'))
+console.log(document.querySelector('img[sign*=bull-cow]'))
