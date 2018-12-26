02da9a4910ee822db590a7ec9bcc0d45092d353d
Rade Bajic <bajic.rade2@gmail.com>
2018-12-26 14:42:51 +0100

    notes

--- css_in_dept/index.js
+++ css_in_dept/index.js
@@ -1,3 +1,13 @@
 console.log(document.querySelector('img[alt="fotografija fotke"]'));
 console.log(document.querySelector('div[nesto|=blah]'));
 console.log(document.querySelector('div[nesto~=sejmur]'));
+
+console.log(document.querySelector('div[nesto|=blah]'));
+console.log(document.querySelectorAll('div[nesto$=filip]')[0]);
+
+console.log('********************************************************');
+
+console.log(document.querySelectorAll('section[nesto^=blah]'));
+console.log(document.querySelectorAll('section[nesto$=ajovaska]'));
+
+console.log(document.querySelector('div[neki*=sand]'))
