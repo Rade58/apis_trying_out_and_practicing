813c40f848fff98d4582eb070c0e5de6b181a179
Rade Bajic <bajic.rade2@gmail.com>
2018-12-31 04:55:47 +0100

    updating notes

--- css_in_dept/index.js
+++ css_in_dept/index.js
@@ -39,3 +39,26 @@ console.log(document.querySelector('.kont_blah_blah ul:nth-child(2)').clientWidt
 console.log(document.querySelector('.kont_blah_blah li:nth-child(1):nth-last-child(2)').clientWidth + document.querySelector('.kont_blah_blah li:nth-child(2):nth-last-child(1)').clientWidth);
 
 console.log(document.querySelector(':root'));
+
+
+const tableEl = document.createElement('table');
+tableEl.classList.add('neka_tabela');
+const tableRow = document.createElement('tr');
+const tableData = document.createElement('td');
+const cellTextNode = document.createTextNode('cell');
+tableData.appendChild(cellTextNode);
+
+for(let i = 0; i < 22; i++){
+
+    let rowClone = tableRow.cloneNode();
+
+    for(let j = 0; j < 18; j++) {
+        rowClone.appendChild(tableData.cloneNode(true));
+    }
+
+    tableEl.appendChild(rowClone);
+}
+
+document.body.append(tableEl);
+
+
