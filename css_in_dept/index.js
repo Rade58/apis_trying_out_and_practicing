74c2ff96dc7713035efe54407d20a8cd9910d98b
Rade Bajic <bajic.rade2@gmail.com>
2019-01-02 00:03:40 +0100

    flag example

--- css_in_dept/index.js
+++ css_in_dept/index.js
@@ -62,3 +62,87 @@ for(let i = 0; i < 22; i++){
 document.body.append(tableEl);
 
 
+document.body.append(document.createElement('br'));
+
+
+const usTable = document.createElement('table');
+const usRow = document.createElement('tr');
+const usCell = document.createElement('td');
+const star = document.createTextNode("\u2605"); // OVO CE MI POSLUZITI ZA ONU TABELU, U KOJU CU DA UMECEM
+                                                // ZVEZDICE (UPAMTI DA KAD KORISTIS UNICODE KARAKTERE
+                                                // JAVASCRIPT-U, ONI SU PREFIKSOVANI SA    \u    )
+usTable.classList.add('us_flag');
+
+for(let i = 0; i < 13; i++){
+    let rowClone = usRow.cloneNode();
+
+    if(i === 0){
+        rowClone.appendChild(usCell.cloneNode()); // UMECEM SAMO JEDNU CELIJU, KOJA CE BITI CONTAINER TABELE
+    }                                             // SA ZVEZDICAMA
+
+    // DEO SA ZVEZDICAMA (KOJI TREBA DO BUDE TAKODJE DRUGA TABELA, TREBA DA ZAUZIMA 7 REDOVA)
+    // ZATO U TOM SLUCAJU NE UMECEM PO 30 CELIJA U PRVIH 7 REDOVA, VEC UMECEM 18
+    // DAKLE PRAVIM PROSTOR ZA JEDNU CELIJU, KOJA CE OBUHVATATI TABELU SA ZVEZDICAMA
+
+    if(i < 7){
+        
+        for(let k = 0; k < 18; k++){
+            rowClone.appendChild(usCell.cloneNode());
+        }
+
+    }else{
+
+        for(let j = 0; j < 30; j++){
+            rowClone.appendChild(usCell.cloneNode());
+        }
+
+    }
+
+    usTable.appendChild(rowClone);
+}
+
+document.body.appendChild(usTable);
+
+// PRISTUPAM PRVOJ CELIJI DA BIH JE PROSIRIO
+let firstCell =  document.querySelector('table.us_flag tr:first-child td:first-child');
+// NA SLEDECI NACIN (DAKLE TREBA DA SE PROSTIRE PREKO 12 KOLONA I 7 REDOVA)
+firstCell.setAttribute('colspan', 12);
+firstCell.setAttribute('rowspan', 7);
+
+// UMETNUCU NOVU TABELA U, POMENUTU PRVU CELIJU
+
+firstCell.append(document.createElement('table'));
+
+// TABELA TREBA DA IMA 9 REDOVA, A KOLONA 12
+
+const nestedTable = firstCell.querySelector('table');
+
+for(let i = 0; i < 9; i++){
+    let tempRow = usRow.cloneNode();
+
+    for(let j = 0; j < 12; j++){
+        tempRow.appendChild(tableData.cloneNode());
+    }
+
+    nestedTable.append(tempRow);
+}
+
+firstCell.style.width = `${100/(30/12)}%`;  // DA IZRACUNAM KOLIKO PROCENATA TREBA DA ZAUZIMA
+// firstCell.style.height = `${100/(13/7)}%`;  
+
+nestedTable.width = '100%';
+nestedTable.height = '100%';
+// SADA MOGU DA SE POSVETIM, UMETANJEM ZVEZDICA
+
+nestedTable.querySelectorAll('tr:nth-of-type(2n + 1) > td:nth-of-type(2n + 1)').forEach(tData => {
+
+    tData.appendChild(star.cloneNode());          // UMECEM ZVEZDICU U SVAKI NEPARNI RED, U SVAKU NEPARNU CELIJU
+
+});
+
+nestedTable.querySelectorAll('tr:nth-of-type(2n) > td:nth-of-type(2n)').forEach(tData => {
+
+    tData.appendChild(star.cloneNode());          // UMECEM ZVEZDICU U SVAKI PARNI RED, U SVAKU PARNU CELIJU
+
+});
+
