0db51be12f2217d8e0a635db249f61e5c54ec45a
Rade Bajic <bajic.rade2@gmail.com>
2019-02-04 22:54:45 +0100

    notes

--- css_in_dept/4. FLEXBOX/fleks.js
+++ css_in_dept/4. FLEXBOX/fleks.js
@@ -1,1 +1,22 @@
 console.log('external script');
+
+customElements.define('my-video', class extends HTMLVideoElement {
+    constructor(){
+        super();
+
+        // const shadow = this.attachShadow({mode: 'open'});
+
+        console.log(this.shadowRoot);
+
+        this.elFunk = this.elFunk.bind(this);
+
+        this.elFunk();
+    }
+
+    elFunk(par){
+        console.log(this.constructor);
+        // console.log(this.shadowRoot);
+    }
+
+}, {extends: 'video'});
+
