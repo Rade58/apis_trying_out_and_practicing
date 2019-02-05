60f7e9ff537b28ff6c8c823febdb89b74dc53db3
Rade Bajic <bajic.rade2@gmail.com>
2019-02-05 18:51:59 +0100

    changing element size with arrows

--- css_in_dept/4. FLEXBOX/fleks.js
+++ css_in_dept/4. FLEXBOX/fleks.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* console.log('external script');
 
 customElements.define('my-video', class extends HTMLVideoElement {
@@ -19,4 +21,76 @@ customElements.define('my-video', class extends HTMLVideoElement {
     }
 
 }, {extends: 'video'}); */
+
+
+
+const resFleksKontejner = document.querySelector('div.kont_fl');
+
+const onKeysResize = function(ev){
+
+    ev.preventDefault();
+
+    const code = ev.code;
+    
+    let isArrow = (code === "ArrowUp" ||
+                    code === "ArrowDown" ||
+                    code === "ArrowLeft" ||
+                    code === "ArrowRight")?true:false;
+
+    // console.log(isArrow);    
+
+    if(!isArrow) return;
+
+    const incr_or_decr = 5;
+
+    const scrollbarSizeX = document.querySelector('div.kont_fl').offsetWidth -
+                           document.querySelector('div.kont_fl').clientWidth -
+                           document.querySelector('div.kont_fl').clientLeft*2;
+    const scrollbarSizeY = document.querySelector('div.kont_fl').offsetHeight -
+                           document.querySelector('div.kont_fl').clientHeight -
+                           document.querySelector('div.kont_fl').clientTop*2;
+
+    if(code === "ArrowUp"){
+        console.log(1);
+        let size = resFleksKontejner.clientHeight;
+        resFleksKontejner.style.height = `${Math.abs(size) + incr_or_decr + scrollbarSizeY}px`;
+        return;
+    }
+    if(code === "ArrowDown"){
+        console.log(2)
+        let size = resFleksKontejner.clientHeight;
+        resFleksKontejner.style.height = `${Math.abs(size) - incr_or_decr + scrollbarSizeY}px`;
+        return;
+    }
+    if(code === "ArrowLeft"){
+        console.log(3)
+        let size = resFleksKontejner.clientWidth;
+        resFleksKontejner.style.width = `${Math.abs(size) - incr_or_decr + scrollbarSizeX}px`;
+        return;
+    }
+    if(code === "ArrowRight"){
+        console.log(4)
+        let size = resFleksKontejner.clientWidth;
+        resFleksKontejner.style.width = `${Math.abs(size) + incr_or_decr + scrollbarSizeX}px`;
+        return;
+    }
+    
+};
+
+const flekKontDown = function(ev){
+    console.log('added');
+    document.body.addEventListener('keydown', onKeysResize, false);
+
+};
+
+const flekKontUp = function(ev){
+
+    document.body.removeEventListener('keydown', onKeysResize);
+
+    console.log("removed");
+
+};
+
+resFleksKontejner.addEventListener('mousedown', flekKontDown, false);
+resFleksKontejner.addEventListener('mouseup', flekKontUp, false);
 
