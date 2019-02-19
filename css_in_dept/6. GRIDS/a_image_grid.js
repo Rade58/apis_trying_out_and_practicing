40064ae1261e73bd93903ff17beea4e86ad33b8f
Rade Bajic <bajic.rade2@gmail.com>
2019-02-19 01:40:09 +0100

    awesome image grid

--- css_in_dept/6. GRIDS/a_image_grid.js
+++ css_in_dept/6. GRIDS/a_image_grid.js
@@ -2,32 +2,42 @@ const checkNaturalSizeAndAddClass = imageInstance => {
     const width = imageInstance.naturalWidth;
     const height = imageInstance.naturalHeight;
 
+    console.log(width, height);
+
     if(height > width){
-        console.log(1);
+        // console.log(1);
         return 'portrait';
     }
 
-    if(width < 2000){
-        console.log(2);
+    if(width < 4000){
+        // console.log(2);
         return 'landscape';
     }
-    console.log(3);
+    // console.log(3);
 
     return 'large';
 }
 
 const createImages = (substringName, imageCount, naturalSizeCheckerAndClassAdder ,gridContainerIdOrClass) => {
     const gridContainer = document.querySelector(gridContainerIdOrClass);
+    const fragment = document.createDocumentFragment();
+
 
     for(let i = 0; i < imageCount; i++){
         let gridItem = document.createElement('div');
         let src = `../images/images_for_grid/${substringName}_${i}.jpg`;
         let image = new Image();
         image.src = src;
-        gridItem.classList.add(naturalSizeCheckerAndClassAdder(image));
+        // gridItem.classList.add(naturalSizeCheckerAndClassAdder(image));
+
+        image.addEventListener('load', () => {gridItem.classList.add(naturalSizeCheckerAndClassAdder(image));})
+
         image.alt = "galery image";
         gridItem.appendChild(image);
-        gridContainer.appendChild(gridItem);
+        fragment.appendChild(gridItem);
+        if(i === imageCount - 1){
+            gridContainer.appendChild(fragment);
+        }
     }
 };
 
