ca935b79e89ef83d9f1cbfa55994187efc7569c2
Rade Bajic <bajic.rade2@gmail.com>
2019-04-20 21:03:17 +0200

    cloning of a Response

--- API-EVI (OBJASNJENA)/! practice (JUNK)/main.js
+++ API-EVI (OBJASNJENA)/! practice (JUNK)/main.js
@@ -1,14 +1,28 @@
-window.navigator.serviceWorker.register('/service-worker.js')
+window.navigator.serviceWorker.register('/service-worker.js');
 
 setTimeout(function(){
-    
-    window.fetch("https://httpbin.org/ip")
-    .then(function(response){
-        console.log(response);
-        console.log(response.statusText);
-        console.log('***********************************');
-        console.log(response.bodyUsed);
-        console.log('***********************************');
-    })
 
-}, 3800);
+    window.fetch("https://httpbin.org/ip") // FETCH-UJEM IP ADRESU
+    .then(function(response){
+
+        console.log(response.bodyUsed);
+
+        response.json()
+        .then(function(data){
+            console.log('podaci:', data);
+        })
+
+    })
+                                                                    // DAKLE ONO STA ZELIM DA VIDIM JESTE
+                                                                    // DA LI JE BODY ISKORISCENO
+                                                                    // NAKO NSTO JE RESPONSE SERVIRAN IZ
+                                                                    // CACHE-A
+                                                                    // (ON CE NARAVNO BITI SERVIRAN IZ CACHE-A, TEK NAKON STO
+                                                                    // SE SERVICE WORKER INSTALIRA I AKTIVIRA,
+                                                                    // NAKO NCEGA CE INTERCEPT-OVATI REQUEST
+                                                                    // (JER SAM JA TAK ODEFINISAO U onfetch HANDLER-U
+                                                                    // SERVICE WORKER-A), I CACHIRATI RESPONSE)
+                                                                    // TEK ONDA CE CACHIRANI RESPONSE BITI STALNO SERVIRAN
+                                                                    // U BUDUCNOSTI
+                                                                    // ALI TO SVE MOGU VIDETI DOLE U CODE-U onfetch HANDLER-A
+}, 2000);
--- API-EVI (OBJASNJENA)/! practice (JUNK)/service-worker.js
+++ API-EVI (OBJASNJENA)/! practice (JUNK)/service-worker.js
@@ -1,7 +1,4 @@
-let cacheName = 'cache1-dynamic';
-
-
-self.addEventListener('activate', function(ev){
+/* self.addEventListener('activate', function(ev){
     ev.waitUntil(
         caches.keys()
         .then(function(caNames){
@@ -12,45 +9,57 @@ self.addEventListener('activate', function(ev){
             })
         })
     )
-})
+}) */
+
+let cacheName = 'cache1-dynamic';
 
 self.addEventListener('fetch', function(ev){
     const url = new URL(ev.request.url);
 
-    //console.log(ev.request);
-    //console.log(url);
+    if(url.origin !== location.origin && url.pathname === '/ip'){   // DAKLE SAMO ZELIM DA HANDLE-UJEM JEDAN REQUEST
 
-    //console.log(url.origin !== location.origin && url.pathname === 'https://httpbin.org/ip');
-
-    //console.log(url.origin);
-    //console.log(location.origin);
-
-    if(url.origin !== location.origin && url.pathname === '/ip'){
         ev.respondWith(
-        self.caches.open(cacheName)
-        .then(function(cache){
-            return cache.match(ev.request)
-            .then(function(posibleResponse){
-                if(posibleResponse){
-                    console.log(posibleResponse);
-                    console.log('RESPONSE JE BIO I CACHEI SERVIRAN JE');
-                    //ev.respondWith(posibleResponse);
+            self.caches.open(cacheName)         //DAKLE OTVARAM CACHE PRI SVAKOM TRIGGERING-U fetch EVENT-A
+            .then(function(cache){
 
-                    return posibleResponse;
-                }else{
-                    return self.fetch(ev.request)
-                    .then(function(resp){
-                        console.log('STAVLJAM RESPONE U CACHE, A SADA ODGOVARAM SA MACKOM');
-                        const init = {status: 200, statusText: 'edamer nije sir, i mackega ne vole'}
-                        const macka = new Response(new Blob(), init);
-                        //ev.respondWith(macka);
-                        cache.put(ev.request, resp);
+                return cache.match(ev.request)
+                .then(function(posibleResponse){      // TRAZIM DA LI JE RESPONSE U
 
-                        return macka;
-                    })
-                }
-            });
-        })
-        )
+                    if(posibleResponse){                // AKO JE BIO U CACHE-U VRSIM NJEGOV SERVING
+                        console.log(posibleResponse);
+                        console.log('RESPONSE JE BIO I CACHE I SERVIRAM GA...');
+
+                        return posibleResponse;
+
+                    }else{                                   // AKO NIJE BIO, FETCHUJEM ZA NOVIM RESPONSE-OM
+                        return self.fetch(ev.request)
+                        .then(function(response){
+
+                            // PROVERAVAM DA LI JE body ISKORISCEN
+                            console.log('before --> BODY IS USED: ', response.bodyUsed);     // --> false   (NIJE KORISCEN)
+
+                            // RESPONSE CACHE-IRAM
+                            // ALI OVOG PUTA CACHE-IRAM KLONIRANI RESPONSE
+                            cache.put(ev.request, response.clone());
+
+                            // OPET PROVERAVAM DA LI JE body KORISCEN, NE BI TREBALO, JER NISAM CAHE-IRAO Response INSTANCU KOJA JE
+                            // PASSED OVOM CALLBACK-U
+                            console.log('after --> BODY IS USED: ', response.bodyUsed);      // --> true   (JESTE KOORISCEN)
+                                                                        //            STO ZNACI DA KADA SAM Response INSTANCU
+                                                                        //            DODAO put METODI, KAO ARGUMENT
+                                                                        //            TO JE UCINILO DA SE body, POMENUTE Response
+                                                                        //            INSTANCE, ZAISTA ISKORISTI
+
+                            // SERVIRANJE RESPONSE, KOJEM JE body ISKORISCENO, NEMA NIKAKVOG EFEKTA, JER NECU MOCI ACCESS-OVATI
+                            // PODACIMA USED body-JA
+                            return response;
+                        })
+                    }
+
+                });
+            })
+        );
+
     }
+
 });
