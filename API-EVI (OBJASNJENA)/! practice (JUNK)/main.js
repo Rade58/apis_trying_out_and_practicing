b55f5c324301875239122f4dfd94f56c975db1cf
Rade Bajic <bajic.rade2@gmail.com>
2019-04-22 17:31:55 +0200

    Blob

--- API-EVI (OBJASNJENA)/! practice (JUNK)/main.js
+++ API-EVI (OBJASNJENA)/! practice (JUNK)/main.js
@@ -1,6 +1,6 @@
 window.navigator.serviceWorker.register('/service-worker.js');
 
-setTimeout(function(){
+/* setTimeout(function(){
 
     window.fetch("https://httpbin.org/ip") // FETCH-UJEM IP ADRESU
     .then(function(response){
@@ -25,4 +25,34 @@ setTimeout(function(){
                                                                     // TEK ONDA CE CACHIRANI RESPONSE BITI STALNO SERVIRAN
                                                                     // U BUDUCNOSTI
                                                                     // ALI TO SVE MOGU VIDETI DOLE U CODE-U onfetch HANDLER-A
-}, 2000);
+}, 2000); */
+
+
+const urlSlike = 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Paris_Metro_construction_03300288-3.jpg';
+
+const imageContainer = document.querySelector('div.image-container');           // OVDE CU INSERT-OVATI SLIKU
+
+fetch(urlSlike)                // FETCH-UJEM SLIKU
+.then(function(response){
+
+    return response.blob();    // RETURN-UJEM Promise, KOJI JE RESOLVED SA, Blob INSTANCOM
+
+})
+.catch(function(err){          // AKO JE NEUSPESAN FETCH IZ fetch MEDODE CE PROIZICI Response KOJI JE REJECTED SA ERROR INSTANCOM
+                               // OVO JE NEKI MOJ NACIN CHAINING-A, KOJ ISAM USVOJIO
+    console.log(err);          // KAKO NE BIH IMAO CALLBACK HELL
+
+})
+.then(function(blobInstanca){       // 
+
+    console.log(blobInstanca instanceof Blob); // --> true
+
+    const image = new Image();
+    image.rel = 'old stuff';
+
+    const url = URL.createObjectURL(blobInstanca);    // PRIMENJUJEM POMENUTU METODU          URL.createObjectUrl
+
+    image.src = url;                                  // UCITAVAM IMAGE
+
+    imageContainer.append(image);                     // INSERT-UJEM JE
+})
